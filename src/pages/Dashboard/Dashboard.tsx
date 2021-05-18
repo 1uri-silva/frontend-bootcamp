954bd1a20049e4b49580495c209169f5e0dd8205
rd-easy <iurirodrigues2200@gmail.com>
2021-05-18 08:34:14 -0300

    create screen Dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,59 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Hola</h1>;
+import logo from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title> Explore repositórios no github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/78041884?v=4"
+            alt="Iuri"
+          />
+          <div>
+            <strong>rd-easy/auth-app</strong>
+            <p>application created with authentication using (useContext)</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/78041884?v=4"
+            alt="Iuri"
+          />
+          <div>
+            <strong>rd-easy/auth-app</strong>
+            <p>application created with authentication using (useContext)</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/78041884?v=4"
+            alt="Iuri"
+          />
+          <div>
+            <strong>rd-easy/auth-app</strong>
+            <p>application created with authentication using (useContext)</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,100 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+export const Title = styled.h1`
+  color: #3a3a3a;
 
-export const Title = styled.div``;
+  line-height: 56px;
+  font-size: 48px;
+  max-width: 450px;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    font-weight: bold;
+    background: #04d361;
+    color: #fff;
+
+    width: 210px;
+    height: 70px;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+
+    background: #fff;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
