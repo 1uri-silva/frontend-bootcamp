23a72089d5df3b7510f55b43055cc62f77756f10
rd-easy <iurirodrigues2200@gmail.com>
2021-05-19 08:28:53 -0300

    error

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,56 +1,74 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import logo from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  fullName: string;
+  description?: string;
+  owner: {
+    login: string;
+    avatarUrl: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Error na busca por esse repositório');
+    }
+  }
+
   return (
     <>
       <img src={logo} alt="Github Explorer" />
       <Title> Explore repositórios no github</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars.githubusercontent.com/u/78041884?v=4"
-            alt="Iuri"
-          />
-          <div>
-            <strong>rd-easy/auth-app</strong>
-            <p>application created with authentication using (useContext)</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://avatars.githubusercontent.com/u/78041884?v=4"
-            alt="Iuri"
-          />
-          <div>
-            <strong>rd-easy/auth-app</strong>
-            <p>application created with authentication using (useContext)</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://avatars.githubusercontent.com/u/78041884?v=4"
-            alt="Iuri"
-          />
-          <div>
-            <strong>rd-easy/auth-app</strong>
-            <p>application created with authentication using (useContext)</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.fullName} href="test">
+            <img
+              src={repository.owner.avatarUrl}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.fullName}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
